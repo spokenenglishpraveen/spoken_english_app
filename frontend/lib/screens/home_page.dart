463f63f9326ef56ba9c184dcf3501843571ce2a3
spokenenglishpraveen <praveen.spoken.english1@gmail.com>
2025-06-02 23:18:34 +0530

    Update home_page.dart

--- frontend/lib/screens/home_page.dart
+++ frontend/lib/screens/home_page.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
+import 'all_tenses_page.dart';
+import 'tensewise_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Spoken English Practice")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text("Practice All Tenses"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AllTensesPage()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text("Tense-wise Practice"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TenseWisePage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
